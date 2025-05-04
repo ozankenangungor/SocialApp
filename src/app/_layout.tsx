498968f581ab7205dfe4ddb38baa3fb486d9f8df
Ozan Kenan Güngör <ozankenangungor@Ozan-MacBook-Pro.local>
2025-05-04 12:19:04 +0300

    Expo Router install & configure

---
+++ src/app/_layout.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Slot, Stack } from 'expo-router'
+
+export default function RootLayout() {
+    return <Stack />
+}
+
+const styles = StyleSheet.create({})
