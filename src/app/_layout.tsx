b85e8ce3dbcd431200c66de86ac22aae60c3fd5e
Ozan Kenan Güngör <ozankenangungor@Ozan-MacBook-Pro.local>
2025-05-04 16:41:19 +0300

    Navigation dark theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,8 +2,22 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Slot, Stack, Tabs } from 'expo-router'
 
+import { ThemeProvider, DarkTheme } from '@react-navigation/native'
+
+const myTheme = {
+    ...DarkTheme,
+    colors: {
+        ...DarkTheme.colors,
+        primary: 'white',
+    }
+}
+
 export default function RootLayout() {
-    return <Slot />
+    return (
+        <ThemeProvider value={myTheme}>
+            <Slot />
+        </ThemeProvider>
+    )
 }
 
 const styles = StyleSheet.create({})
