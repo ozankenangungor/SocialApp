ba0af38e7a949bb3bd8e42177c0f1aa8df4dc69f
Ozan Kenan Güngör <ozankenangungor@Ozan-MacBook-Pro.local>
2025-05-17 15:15:11 +0300

    create post tab button

--- src/app/(protected)/(tabs)/_layout.tsx
+++ src/app/(protected)/(tabs)/_layout.tsx
@@ -1,28 +1,49 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { Stack, Tabs } from 'expo-router'
+import { Tabs, router } from 'expo-router'
 import Feather from '@expo/vector-icons/Feather';
 
 export default function TabsLayout() {
-    return <Tabs screenOptions={{ tabBarActiveTintColor: 'black', tabBarShowLabel: false }}>
-        <Tabs.Screen name="index"
-            options={{
-                title: 'Home',
-                tabBarIcon: ({ size, color }) => <Feather name="home" size={size} color={color} />
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+            }}
+        >
+            <Tabs.Screen name="index"
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({ size, color }) => <Feather name="home" size={size} color={color} />
+                }} />
+            <Tabs.Screen name="search" options={{
+                title: 'Search',
+                tabBarIcon: ({ size, color }) => <Feather name="search" size={size} color={color} />
             }} />
-        <Tabs.Screen name="search" options={{
-            title: 'Search',
-            tabBarIcon: ({ size, color }) => <Feather name="search" size={size} color={color} />
-        }} />
-        <Tabs.Screen name="notifications" options={{
-            title: 'Notifications',
-            tabBarIcon: ({ size, color }) => <Feather name="heart" size={size} color={color} />
-        }} />
-        <Tabs.Screen name="profile" options={{
-            title: 'Profile',
-            tabBarIcon: ({ size, color }) => <Feather name="user" size={size} color={color} />
-        }} />
-    </Tabs>
+            <Tabs.Screen
+                name='plus'
+                options={{
+                    title: 'Plus',
+                    tabBarIcon: ({ size, color }) => (
+                        <Feather name='plus' size={size} color={color} />
+                    ),
+                }}
+                listeners={{
+                    tabPress: (e) => {
+                        e.preventDefault();
+                        router.push('/new');
+                    },
+                }}
+            />
+            <Tabs.Screen name="notifications" options={{
+                title: 'Notifications',
+                tabBarIcon: ({ size, color }) => <Feather name="heart" size={size} color={color} />
+            }} />
+            <Tabs.Screen name="profile" options={{
+                title: 'Profile',
+                tabBarIcon: ({ size, color }) => <Feather name="user" size={size} color={color} />
+            }} />
+        </Tabs>
+    );
 }
 
 const styles = StyleSheet.create({})
